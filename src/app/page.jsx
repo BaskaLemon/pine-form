a9ddd98f657de018479c93fb52aa7014f12e4e18
BaskaLemon <erdenebayarbaasandorj89@gmail.com>
2026-04-07 11:20:56 +0800

    save logic

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FirstStep } from "./components/FirstStep";
 import { SecondStep } from "./components/SecondStep";
 import { ThirdStep } from "./components/ThirdStep";
@@ -16,28 +16,8 @@ export default function Home() {
   const prevStep = () => {
     setCurrentStep((prev) => prev - 1);
   };
-  const [form, setForm] = useState({
-    firstname: "",
-    lastname: "",
-    username: "",
-    email: "",
-    phone: "",
-    password: "",
-    confirmPass: "",
-    date: "",
-    picture: "",
-  });
-  const [errors, setErrors] = useState({
-    firstname: "",
-    lastname: "",
-    username: "",
-    email: "",
-    phone: "",
-    password: "",
-    confirmPass: "",
-    date: "",
-    picture: "",
-  });
+  const [form, setForm] = useState(null);
+  const [errors, setErrors] = useState(null);
   const [touched, setTouched] = useState({
     firstname: false,
     lastname: false,
@@ -49,7 +29,78 @@ export default function Home() {
     date: false,
     picture: false,
   });
-
+  useEffect(() => {
+    if (form !== null) {
+      localStorage.setItem("form", JSON.stringify(form));
+    } else {
+      const storedForm = JSON.parse(localStorage.getItem("form"));
+      if (storedForm) {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setForm(storedForm);
+      } else {
+        setForm({
+          firstname: "",
+          lastname: "",
+          username: "",
+          email: "",
+          phone: "",
+          password: "",
+          confirmPass: "",
+          date: "",
+          picture: "",
+        });
+      }
+    }
+  }, [form]);
+  useEffect(() => {
+    if (errors !== null) {
+      localStorage.setItem("errors", JSON.stringify(errors));
+    } else {
+      const storedError = JSON.parse(localStorage.getItem("errors"));
+      if (storedError) {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setErrors(storedError);
+      } else {
+        setErrors({
+          firstname: "",
+          lastname: "",
+          username: "",
+          email: "",
+          phone: "",
+          password: "",
+          confirmPass: "",
+          date: "",
+          picture: "",
+        });
+      }
+    }
+  }, [errors]);
+  useEffect(() => {
+    if (touched !== null) {
+      localStorage.setItem("touched", JSON.stringify(touched));
+    } else {
+      const storedTouched = JSON.parse(localStorage.getItem("touched"));
+      if (storedTouched) {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
+        setErrors(storedTouched);
+      } else {
+        setErrors({
+          firstname: false,
+          lastname: false,
+          username: false,
+          email: false,
+          phone: false,
+          password: false,
+          confirmPass: false,
+          date: false,
+          picture: false,
+        });
+      }
+    }
+  }, [touched]);
+  if (!form) return null;
+  if (!errors) return null;
+  if (!touched) return null;
   return (
     <div className="flex flex-col justify-center items-center h-screen w-full">
       <div className="flex flex-col w-120 h-fit p-8 gap-6 justify-between items-start shadow-2xl">
